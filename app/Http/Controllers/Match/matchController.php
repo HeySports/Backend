8aad02403e946812ed074f4bb865ff9badfebb00
ThanhDoanDev <thanhdoan2000pn@gmail.com>
2021-04-08 14:53:25 +0700

    fix model hsitories search

--- app/Http/Controllers/Match/matchController.php
+++ app/Http/Controllers/Match/matchController.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Matches;
 use App\Models\DetailMatch;
-use App\models\HistoriesSearch;
+use App\Models\HistoriesSearch;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Auth;
 use Validator;
