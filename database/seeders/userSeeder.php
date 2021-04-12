95d460d53d3fd37ede0ca39d932c58f135d9b88e
ThanhDoanDev <thanhdoan2000pn@gmail.com>
2021-04-12 17:18:43 +0700

    postion

--- database/seeders/userSeeder.php
+++ database/seeders/userSeeder.php
@@ -18,7 +18,9 @@ class userSeeder extends Seeder
         $_listName=['Thanh Doan','Nguyen Hung','Yen Nhi', 'Ngoc Tram','Nguyen Van A', 'Nguyen Van B','Nguyen Van C', 'Nguyen Van D','Nguyen Van E', 'Nguyen Van F'];
         $_email=['thanhdoanDev@gmail.com','hung@gmail.com','Nhi@gmail.com','tram@gmail.com','a@gmail.com','b@gmail.com','c@gmail.com','d@gmail.com','e@gmail.com','f@gmail.com'];
         $_phone=['0946613608','0946613610','0946613600','0946613607','0946613606','0946613605','0946613604','0946613603','0946613602','0946613601'];
+        $_position=['Tiền Đạo','Tiền Vệ','Hậu Vệ'];
         for ($i=0; $i<10; $i++){
+            $r=rand(0,2);
             DB::table('users')->insert([
                 'id_roles' => rand(1, 3),
                 'full_name' =>  $_listName[$i],
@@ -31,7 +33,7 @@ class userSeeder extends Seeder
                 'matches_number' => rand(0,20),
                 'skill_rating' => rand(0,5),
                 'attitude_rating' => rand(0,5),
-                'position_play' => Str::random(10),
+                'position_play' => $_position($r),
                 'description' =>  Str::random(50),
             ]);
         }
