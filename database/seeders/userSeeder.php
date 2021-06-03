6f894ab2e07eb2926e74150bd1fc505b5cae6964
Hung Nguyen <hungnguyen@kms-technology.com>
2021-06-03 18:29:31 +0700

    add long lat user

--- database/seeders/userSeeder.php
+++ database/seeders/userSeeder.php
@@ -16,6 +16,12 @@ class userSeeder extends Seeder
     public function run()
     {
         $address = ['101b Lê Hữu Trác - Sơn Trà - Đà Nẵng', '99 Tô Hiến Thành - Sơn Trà - Đà Nẵng'];
+        $long=[16068939,16073887, 16046397, 16035003, 16048606, 16051262,16031422, 16049999
+        ,16046966, 16053344, 160071391, 16055177,16050691
+        ,16066359,16077608   ,16024408
+        ,16019858 ,16057495, 16071960 ];
+        $lat=[108170022 ,108177318 ,108214454 ,108226816 ,108210098 ,108211693 ,108207945 ,108215703 ,108211627 ,
+        108206614 ,108183363 ,108174595 ,108183668 ,108194122 ,108204987 ,108224250 ,108204066 ,108234181 ,108180323];
         $_listName=['Thanh Doan','Nguyen Hung','Yen Nhi', 'Ngoc Tram','Nguyen Van A', 'Nguyen Van B','Nguyen Van C', 'Nguyen Van D','Nguyen Van E', 'Nguyen Van F'];
         $_email=['thanhdoanDev@gmail.com','hung@gmail.com','Nhi@gmail.com','tram@gmail.com','a@gmail.com','b@gmail.com','c@gmail.com','d@gmail.com','e@gmail.com','f@gmail.com'];
         $_phone=['0946613655','0946613610','0946613600','0946613607','0946613606','0946613605','0946613604','0946613603','0946613602','0946613601'];
@@ -29,6 +35,8 @@ class userSeeder extends Seeder
                 'phone_numbers' => $_phone[$i],
                 'address' => 'Son Tra, Da Nang',
                 'avatar' => '[]',
+                'longitude'=>$long[rand(0,18)],
+                'latitude'=> $lat[rand(0,18)],
                 'age' => rand(10,60),
                 'matches_number' => rand(0,20),
                 'skill_rating' => rand(0,10),
@@ -47,6 +55,8 @@ class userSeeder extends Seeder
                 'address' => $address[rand(0,1)],
                 'avatar' => '[]',
                 'age' => rand(10,60),
+                'longitude'=>$long[rand(0,18)],
+                'latitude'=> $lat[rand(0,18)],
                 'matches_number' => rand(0,20),
                 'skill_rating' => rand(0,10),
                 'attitude_rating' => rand(0,5),
