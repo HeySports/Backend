992808135a33e6eb9daefe32669a352ade91ab6f
Doan Thanh <thanhdoandev@gmail.com>
2021-06-05 12:11:33 +0700

    change image

--- database/seeders/TeamSeeder.php
+++ database/seeders/TeamSeeder.php
@@ -21,8 +21,8 @@ class TeamSeeder extends Seeder
         $_team=[
             'https://bongdadoisong.com/wp-content/uploads/2018/11/45245933_327307784713580_854832593667883008_n-1024x550.jpg',
         'https://bongdadoisong.vn/uploads/news/2020/05/27/anh-em-phui-di-an.jpg',
-        'https://lh3.googleusercontent.com/proxy/bUwLSOBI-CdiCvk_hawKQYF0plysOBssrMfW9t_ggxHejOJdhDzmTYpSIr-u5psTf-AkyJEDucIz2BM2XxAfX64iZq5-hTdwHWrv', 
-        'https://lh3.googleusercontent.com/proxy/ooPOYHTpc0bNU5lMKn0OumQkVm3RvwA_kIvmj1uPIxCFoG-Ad9BOEDiErh1l1n68tnas1vgbUt79UVoc5WIY0xLezyfwnePfuyzWhqnVEkgfZ4jgV3DySw',
+        'https://vietfootball.vn/data/uploads/2016/08/tra.jpg', 
+        'http://baokhanhhoa.vn/dataimages/201807/original/images5338443_IMG_57951111.jpg',
         'https://file.hstatic.net/1000261193/file/bulbal_prime_mufc_cup_lan_v_2_b811a785e80641b6a6430ceba03efc5f_grande.jpg'];
         for ($i = 0; $i < 10; $i++){
             DB::table('teams')->insert([
